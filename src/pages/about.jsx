b45576a3d9296078ea75e1df7460e7517ff8b21b
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-04-24 17:02:56 -0700

    Added about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,9 +12,21 @@ export default function Home() {
             <NavBar />
 
             <main>
-                <div>About Me</div>
+                <div>
+                    <h2>Software Developer that's always learning and creating</h2>
+                    <p>
+                        I'm Jose, a Computer Science graduate from the University of Nevada, Las Vegas.
+                        I first began programming in 2014-2015 during my sophomore year in the Video Game Design
+                        program at Southwest Career and Technical Academy. I've kept on learning and improving my abilities since then.
+                        I'm always pushing myself to be better whether it's mentally with programming or physically in the gym.
+                    </p>
+                </div>
                 <div>Professional Summary</div>
-                <div>Accolades</div>
+                <div>
+                    <p>Accolades</p>
+                    <img src={null} alt="1st Place Award" />
+                    <img src={null} alt="Sustainability Award" />
+                </div>
                 <a href="https://drive.google.com/file/d/138lc0DMG8xH6Ih0mDE89AgiqvoZfDtVu/view?usp=sharing">Resume</a>
             </main>
 
