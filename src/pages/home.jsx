04cbd72211ddb1938e08475a8d4e3249108cf07f
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-05-09 16:14:52 -0700

    Added styling to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -13,16 +13,16 @@ export default function Home() {
             <NavBar />
 
             <main>
-                <div>
+                <div className='centered-text'>
                     <h2>Software Developer - Full Stack</h2>
                     <p>Always move forward. Never stop learning.</p>
                 </div>
                 
                 <div>
-                    <h3>Projects</h3>
+                    <h3 className='centered-text'>Projects</h3>
 
-                    <div>
-                        <h4><a href="https://zzz-planner.onrender.com/login">Zenless Zone Zero Upgrade Planner</a></h4>
+                    <div className='zzz_planner centered-text'>
+                        <h4><a href="https://zzz-planner.onrender.com/login">Zenless Zone Zero Progress Planner</a></h4>
 
                         <div className='zzz_sc'>
                             <img src="/zzz-planner-agents.png" alt="Screenshot" />
@@ -33,7 +33,7 @@ export default function Home() {
                         
                         <p>Progress planner for the game Zenless Zone Zero. Stay organized while progressing through the game.</p>
 
-                        <div>
+                        <div className='repos'>
                             <a href="https://github.com/sisonj3/zzz-planner-client.git">Front End Repository</a>
                             <a href="https://github.com/sisonj3/zzz-planner-api.git">Back End Repository</a>
                         </div>
